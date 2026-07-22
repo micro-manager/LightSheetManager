bee90eff70246c2959c53c72a97ad61c0d899781
Brandon <brandon@asiimaging.com>
2026-07-22 02:47:15 -0700

    update version

--- src/main/java/org/micromanager/lightsheetmanager/LightSheetManagerPlugin.java
+++ src/main/java/org/micromanager/lightsheetmanager/LightSheetManagerPlugin.java
@@ -14,7 +14,7 @@ public class LightSheetManagerPlugin implements MenuPlugin, SciJavaPlugin {
     public static final String copyright = "Applied Scientific Instrumentation (ASI), 2022-2026";
     public static final String description = "A plugin to control various types of light sheet microscopes.";
     public static final String menuName = "Light Sheet Manager";
-    public static final String version = "0.7.4";
+    public static final String version = "0.7.5";
 
     private Studio studio_;
     private LightSheetManager model_;
